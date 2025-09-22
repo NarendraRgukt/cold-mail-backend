d0d88615e1806315d6b3c64e64fac18c32621379
NarendraRgukt <jamesharrypotter8@gmail.com>
2025-09-22 22:23:00 +0530

    Added email filed to the employee

--- employees/models.py
+++ employees/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 
 class Employee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True, null=True)
     company_name = models.CharField(max_length=255)
@@ -10,6 +10,7 @@ class Employee(models.Model):
     linkedin_profile = models.URLField(blank=True, null=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    email=models.EmailField(unique=True,default="examle@gmail.com")
 
     def __str__(self):
         return f"{self.first_name} - {self.company_name}"
