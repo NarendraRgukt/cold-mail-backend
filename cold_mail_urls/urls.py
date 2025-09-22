437750abb0098044d56350387155038fb0526c84
NarendraRgukt <jamesharrypotter8@gmail.com>
2025-09-22 22:48:47 +0530

    Added api for the bulk upload and listing out the all the employees

--- cold_mail_urls/urls.py
+++ cold_mail_urls/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from employees.views import EmployeeView,BulkEmployeeUploadView
+from employees.views import EmployeeView,BulkEmployeeUploadView,EmployeeListView
 
 urlpatterns = [
     path("employees/", EmployeeView.as_view()),             # POST
     path("employees/<uuid:pk>/", EmployeeView.as_view()), 
-     path("employees/bulk-upload/", BulkEmployeeUploadView.as_view()),  # PUT, DELETE
+     path("employees/bulk-upload/", BulkEmployeeUploadView.as_view()), 
+      path("get/employees/", EmployeeListView.as_view(), name="employee-list"),
 ]
